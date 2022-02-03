986f47e5fecd2decc1bb3067e417e55235218ad0
mbaye <mbayesarr.baba@gmail.com>
2022-02-03 00:05:36 +0000

    Initial commit

---
+++ src/test/java/com/example/demofacebooktdd/DemoFacebookTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofacebooktdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFacebookTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
